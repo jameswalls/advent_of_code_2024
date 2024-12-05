17691b4fdb09c9d0727e570de950ba9fe077ed98
james walls <jjpp10909@gmail.com>
2024-12-05 01:13:42 -0600

    day 05 part 2

--- days/05/part1.py
+++ days/05/part1.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import pdb
 
 def load(path: str) -> tuple[defaultdict, list[list]]:
     with open(path, "r") as file:
@@ -33,15 +34,32 @@ def sum_middle_valid_pages(rules: defaultdict, updates: list[list]) -> tuple[int
 
     return sum(middle_pages), invalid
 
+def sum_middle_fixed_pages(rules: defaultdict, updates: list[list]) -> int:
+    fixed_updates = []
+    for update in updates:
+        seen_pages = []
+        for page in update:
+            if rule_pages := rules[page].intersection(seen_pages):
+                insert_idx = min(seen_pages.index(page) for page in rule_pages)
+                seen_pages.insert(insert_idx, page)
+            else:
+                seen_pages.append(page)
+        fixed_updates.append(seen_pages)
+
+    return sum(update[len(update)//2] for update in fixed_updates)
+
 print(40*"-")
 print("test")
 rules, updates = load("./test.txt")
-total = sum_middle_valid_pages(rules, updates)
+total, invalid = sum_middle_valid_pages(rules, updates)
+total_fixed = sum_middle_fixed_pages(rules, invalid)
 print(f"total: {total}")
-
+print(f"total fixed: {total_fixed}")
 
 print(40*"-")
 print("part 1")
 rules, updates = load("./input.txt")
-total = sum_middle_valid_pages(rules, updates)
+total, invalid = sum_middle_valid_pages(rules, updates)
+total_fixed = sum_middle_fixed_pages(rules, invalid)
 print(f"total: {total}")
+print(f"total fixed: {total_fixed}")
