c9ad2f534d0e82f4294c166529c5324f1717cdb6
james walls <35391305+jameswalls@users.noreply.github.com>
2024-12-18 23:52:40 -0600

    day 7 part 1

---
+++ days/07/scripty.py
@@ -0,0 +1,60 @@
+import pdb
+from abc import ABC, abstractmethod
+from itertools import product
+from typing import Iterator, Callable
+
+with open("./part1.txt") as file:
+    equations: list[tuple[int, list[int]]] = []
+    for line in file.readlines():
+        result, operands = line.split(":")
+        equations.append((int(result), [int(i) for i in operands.strip().split()]))
+
+class Operation(ABC):
+    @abstractmethod
+    def __call__(self, x: int, y: int) -> int: ...
+
+class Add(Operation):
+    def __str__(self) -> str:
+        return "+"
+
+    def __repr__(self) -> str:
+        return "+"
+
+    def __call__(self, x: int, y: int) -> int:
+        return x + y
+
+
+class Mult(Operation):
+    def __str__(self) -> str:
+        return "*"
+
+    def __repr__(self) -> str:
+        return "*"
+
+    def __call__(self, x: int, y: int) -> int:
+        return x * y
+
+def reduce(operands: list[int], operations: list[Operation], value: int|None=None) -> int:
+    if not value:
+        return reduce(operands[1:], operations, operands[0])
+    elif operations:
+        next_operand = operands[0]
+        f = operations[0]
+        return reduce(operands[1:], operations[1:], f(value, next_operand))
+    else:
+        return value
+        
+add = Add()
+mult = Mult()
+
+total_calibration_result = 0
+for result, operands in equations:
+    for operations in product([add, mult], repeat=len(operands)-1):
+        o = operands.copy()
+        print(operands, operations)
+        output = reduce(operands, list(operations))
+        if result == output:
+            total_calibration_result += result
+            break
+
+print(f"total_calibration_result: {total_calibration_result}")
